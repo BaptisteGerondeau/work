c68be6c7f15d2f59dd411ec3996182355de0bfbc
Rafael David Tinoco <rafael.tinoco@linaro.org>
2018-08-29 00:00:13 -0300

    bug3950: added latest reproducer

--- notes/bugs/3950/reproducer.sh
+++ notes/bugs/3950/reproducer.sh
@@ -28,16 +28,40 @@ hugeadm --thp-never
 # do_test("heapshrink", LD_PRELOAD="libhugetlbfs.so libheapshrink.so", HUGETLB_MORECORE="yes", HUGETLB_MORECORE_SHRINK="yes")
 #
 
+#export HUGETLB_VERBOSE=99
+
+export NEWLINKER=/opt/libc6-preload/lib/ld-linux-x86-64.so.2
+
 export LD_LIBRARY_PATH=/opt/libhugetlbfs/tests/obj64
 
+sudo chmod +x $NEWLINKER
+
 PROGRAM="/opt/libhugetlbfs/tests/obj64/heapshrink"
+#PROGRAM="/opt/libhugetlbfs/tests/obj64/heap-overflow"
 
-# LD_PRELOAD=libhugetlbfs.so = dynamic linker will load libhugetlbfs shared library libhugetlbfs.so
-# LD_PRELOAD= = dynamic linker will load libhugetlbfs shared library libhugetlbfs.so
+#PREFIX="$NEWLINKER --library-path /opt/libc6-preload/lib/x86_64-linux-gnu:/opt/libc6-preload/lib/:/lib:/lib/x86_64-linux-gnu/:/lib64"
+PREFIX=""
+PROGRAM="$PREFIX $PROGRAM"
 
-$PROGRAM
-LD_PRELOAD="libheapshrink.so" $PROGRAM
-LD_PRELOAD="libhugetlbfs.so" HUGETLB_MORECORE=yes $PROGRAM
-LD_PRELOAD="libhugetlbfs.so libheapshrink.so" HUGETLB_MORECORE=yes $PROGRAM
-LD_PRELOAD="libheapshrink.so" HUGETLB_MORECORE=yes HUGETLB_MORECORE_SHRINK=yes $PROGRAM
-LD_PRELOAD="libhugetlbfs.so libheapshrink.so" HUGETLB_MORECORE_SHRINK=yes HUGETLB_MORECORE=yes $PROGRAM
+# no libhugetlbfs being used
+#
+# $PROGRAM
+# HUGETLB_MORECORE=yes $PROGRAM
+# HUGETLB_MORECORE=yes HUGETLB_MORECORE_SHRINK=yes $PROGRAM
+
+# libhugetlbfs shrink tests
+#
+# $PROGRAM
+# LD_PRELOAD="libheapshrink.so" $PROGRAM
+# LD_PRELOAD="libhugetlbfs.so" HUGETLB_MORECORE=yes $PROGRAM
+# LD_PRELOAD="libhugetlbfs.so libheapshrink.so" HUGETLB_MORECORE=yes $PROGRAM
+# LD_PRELOAD="libheapshrink.so" HUGETLB_MORECORE=yes HUGETLB_MORECORE_SHRINK=yes $PROGRAM
+# LD_PRELOAD="libhugetlbfs.so libheapshrink.so" HUGETLB_MORECORE_SHRINK=yes HUGETLB_MORECORE=yes $PROGRAM
+
+# WORKAROUND (or fix ?)
+
+# export GLIBC_TUNABLES=glibc.malloc.tcache_count=0
+
+# REAL SHRINK TEST
+LD_PRELOAD="libhugetlbfs.so libheapshrink.so" HUGETLB_MORECORE=yes HUGETLB_MORECORE_SHRINK=yes $PROGRAM
+# LD_PRELOAD="libhugetlbfs.so" HUGETLB_MORECORE=1G $PROGRAM
