90397a1ac4768b91e8249e31f64d57d2a43f79c7
Rafael David Tinoco <rafael.tinoco@linaro.org>
2018-11-05 08:50:26 -0200

    2018-11-05 update

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,7 +17,7 @@ NCPU=$(($NUMCPU - 1))
 #NCPU=1
 
 MYARCH="amd64"  # (amd64|x86|arm64|armhf|armel)
-TOARCH="armhf"  # (amd64|x86|arm64|armhf|armel)
+TOARCH="arm64"    # (amd64|x86|arm64|armhf|armel)
 
 KCROSS=0        # are you cross compiling ? (automatic)
 if [ "$MYARCH" != "$TOARCH" ]; then
@@ -33,7 +33,7 @@ KMCONFIG=0      # want a menu to add/remove stuff from .config ? (default: 0)
 KLCONFIG=0      # want to merge a lsmod file into .config ? (default: 0)
 KPREPARE=0      # want to prepare ? (default: 1)
 KBUILD=1        # want to build ? :o) (default: 1)
-KDEBUG=1        # want your kernel to have debug symbols ? (default: 1)
+KDEBUG=0        # want your kernel to have debug symbols ? (default: 1)
 KVERBOSE=0      # want it to shut up ? (default: 1)
 
 KRAMFS=0        # TARGET will be a KRAMFSSIZE GB tmpfs (default: 0)
@@ -54,10 +54,10 @@ AMD64CONFIG="$FILEDIR/config-amd64"
 
 # BOARDS (ENABLE HERE)
 
-DRAGON=0        # dragon board config file (default: 0)
-HIKEY=0         # hikey board config file (default: 0)
-BEAGLE=1        # beable board config file (default: 0)
-OTHER=0         # some other config file (default: 0)
+DRAGON=0        # arm64 - dragon board config file (default: 0)
+HIKEY=1         # arm64 - hikey board config file (default: 0)
+BEAGLE=0        # armhf - beable board config file (default: 0)
+OTHER=0         # xxxxx - some other config file (default: 0)
 
 DRAGONCONFIG="$FILEDIR/config-dragon"
 HIKEYCONFIG="$FILEDIR/config-hikey"
