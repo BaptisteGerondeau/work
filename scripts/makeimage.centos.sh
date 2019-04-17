b356b370ac9483885b411689d557a45d6e098f24
Baptiste GERONDEAU <bgerondeau@gmail.com>
2019-04-17 16:31:38 +0200

    WIP Refactor

---
+++ scripts/makeimage.centos.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+### DO NOT CALL FIRST : Call "scripts/makeguest.sh centos"
+rpm --root $TARGET --initdb
+rpm -ivh --force-debian --nodeps --root $TARGET $CENTOS_RELEASE_RPM_URL
+ln -s $TARGET/etc/pki /etc/pki
+yum --installroot $TARGET -y install yum passwd shadow shadow-utils iputils iproute
+
+mount -o bind /dev $TARGET/dev
+mount -o bind /dev/pts $TARGET/dev/pts
+mount -o bind /sys $TARGET/sys
+mount -o bind /proc $TARGET/proc
+
+echo $HOSTNAME | tee $TARGET/etc/hostname
+
+chroot $TARGET /bin/bash -c "passwd -d root"
+chroot $TARGET /bin/bash -c "echo root:root | chpasswd"
+
+chroot $TARGET /bin/bash -c "useradd -d /home/inaddy -m -s /bin/bash inaddy"
+chroot $TARGET /bin/bash -c "passwd -d inaddy"
+chroot $TARGET /bin/bash -c "echo inaddy:inaddy | chpasswd"
+
+chroot $TARGET /bin/bash -c "useradd -d /home/bgerdeb -m -s /bin/bash bgerdeb"
+chroot $TARGET /bin/bash -c "passwd -d bgerdeb"
+chroot $TARGET /bin/bash -c "echo bgerdeb:bgerdeb | chpasswd"
+
+echo """## /etc/sudoers
+
+Defaults env_keep += \"LANG LANGUAGE LINGUAS LC_* _XKB_CHARSET\"
+Defaults env_keep += \"HOME EDITOR SYSTEMD_EDITOR PAGER\"
+Defaults env_keep += \"XMODIFIERS GTK_IM_MODULE QT_IM_MODULE QT_IM_SWITCHER\"
+
+Defaults secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"
+
+Defaults logfile=/var/log/sudo.log,loglinelen=0
+Defaults !syslog, !pam_session
+
+root ALL=(ALL) NOPASSWD: ALL
+%wheel ALL=(ALL) NOPASSWD: ALL
+%sudo ALL=(ALL) NOPASSWD: ALL
+inaddy ALL=(ALL) NOPASSWD: ALL
+bgerdeb ALL=(ALL) NOPASSWD: ALL
+
+## end of file""" | tee $TARGET/etc/sudoers
+
+echo """## /etc/fstab
+
+LABEL=ROOT / ext4 errors=remount-ro 0 1
+
+## end of file""" | tee $TARGET/etc/fstab
+
+echo """## /etc/sysconfig/network
+
+NETWORKING=yes
+
+## end of file""" | tee $TARGET/etc/sysconfig/network
+
+echo """## /etc/resolv.conf
+
+nameserver 10.40.0.1
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+
+## end of file""" | tee $TARGET/etc/resolv.conf
+
+ARCH=$(uname -r | cut -d'-' -f3)
+
+chroot $TARGET /bin/bash -c "rpm --initdb"
+chroot $TARGET /bin/bash -c "yum clean all"
+chroot $TARGET /bin/bash -c "yum --releasever=7 install -y yum centos-release"
+chroot $TARGET /bin/bash -c "yum --releasever=7 install -y @core @base kernel kernel-headers redhat-lsb-core dracut-tools dracut-config-generic dracut-config-rescue ${PACKAGES//,/ }"
+chroot $TARGET /bin/bash -c "$LOCALE"
